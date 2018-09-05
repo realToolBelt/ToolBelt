d5a0d88107e6e5039e4df2014070ba70f96080b3
Eugene Pawlik <drew.pawlik@gmail.com>
2018-09-05 14:24:13 -0500

    Update layout of the custom image cell. Add binding proxy to prepare for upcoming bindings. Begin adding support for long presses on list-view items on android.

---
+++ src/ToolBelt/Models/BindingProxy.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace ToolBelt.Models
+{
+    public class BindingProxy : BindableObject
+    {
+        public static readonly BindableProperty DataProperty = BindableProperty.Create(
+            nameof(Data),
+            typeof(object),
+            typeof(BindingProxy),
+            null,
+            BindingMode.OneWay);
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+    }
+}
