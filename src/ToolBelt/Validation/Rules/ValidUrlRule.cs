d64faf6fc8de0bff0b4822f20c346d1d30a774e9
Eugene Pawlik <drew.pawlik@gmail.com>
2018-09-10 08:26:11 -0500

    Added a URL validation rule to prepare for "contractor URL" functionality.

---
+++ src/ToolBelt/Validation/Rules/ValidUrlRule.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolBelt.Validation.Rules
+{
+    /// <summary>
+    /// A validation rule that verifies that a value is a valid URL.
+    /// </summary>
+    /// <seealso cref="ToolBelt.Validation.IValidationRule{string}" />
+    public class ValidUrlRule : IValidationRule<string>
+    {
+        private static readonly UrlAttribute _validationAttribute = new UrlAttribute();
+
+        public string ValidationMessage
+        {
+            get;
+            set;
+        } = "Should be a URL";
+
+        public bool IsValid(string value)
+        {
+            return _validationAttribute.IsValid(value);
+        }
+    }
+}
